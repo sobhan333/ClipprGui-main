9f2470efb6189467ee09b8c1a63bc5b19014a1ff
Sobhan333 <ghasemi.sobhan1384@gmail.com>
2024-03-26 03:28:02 -0700

    fixed submit, added loading screen, fixed dark mode cleaned up css and html a lil

--- Main.js
+++ Main.js
@@ -1,6 +1,6 @@
 async function connectsocket() {
   const exampleSocket = new WebSocket("wss://s2zwrp5xhd.execute-api.us-west-2.amazonaws.com/production/");
-
+  showLoader()
   checksocket(exampleSocket)
 
     await new Promise(resolve => setTimeout(resolve, 3000));
@@ -14,9 +14,10 @@ async function connectsocket() {
     
     await new Promise(resolve => setTimeout(resolve, 2000));
     exampleSocket.close()
+    hideLoader()
     checksocket(exampleSocket)
 
-    
+
 }
 
 function getlink(exampleSocket){
@@ -30,5 +31,33 @@ function checksocket(exampleSocket){
     console.log("The Socket is open")
   } else {
     console.log("The Socket has now closed")
+  }
+}
+
+function hideLoader() {
+  var loader = document.getElementById("loader");
+  document.querySelector(".loader").classList.add("loader--hidden");
+}
+
+function showLoader() {
+  var loader = document.getElementById("loader");
+  document.querySelector(".loader").classList.remove("loader--hidden");
+}
+
+function toggleDarkMode() {
+  var body = document.body;
+  var footer = document.querySelector(".aboutus")
+  // Check if dark mode is enabled
+  var isDarkMode = body.classList.contains('dark-mode');
+
+
+  // Toggle dark mode
+  if (isDarkMode) {
+      // If dark mode is enabled, remove it
+      body.classList.remove('dark-mode');
+      footer.classList.remove('.aboutusdark')
+  } else {
+      // If dark mode is not enabled, add it
+      body.classList.add('dark-mode');
   }
 }
