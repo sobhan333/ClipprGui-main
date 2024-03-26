a121372b91c19bdfc8fd732a6ccde36e9c85ffd1
sobhan ghasemi <ghasemi.sobhan1384@gmail.com>
2024-03-25 19:22:43 -0700

    working websocet connection

--- Test.js
+++ Test.js
@@ -5,14 +5,20 @@ async function connectsocket() {
     
     console.log(exampleSocket.readyState)
     await new Promise(resolve => setTimeout(resolve, 3000));
-    exampleSocket.send({"action": "SendMessage"});
+    // exampleSocket.send({action: "SendMessage"});
+    exampleSocket.send(JSON.stringify({action: "SendMessage"}));
     console.log(exampleSocket.readyState)
+    console.log(exampleSocket.onmessage)
+    console.log(exampleSocket.event)
 
 
     exampleSocket.onmessage = (event) => {
         console.log("he2")    
         console.log(event.data);
       };
+    
+        // exampleSocket.close()
+    console.log(exampleSocket.readyState)
 }
 
 function printOutput() {
